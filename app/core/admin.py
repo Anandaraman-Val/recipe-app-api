60d27ba1c30e4502cd25a107a6c3e0efd57698ff
Anandaraman-Val <anandaramanvallinayagam@gmail.com>
2024-08-03 19:43:59 -0700

    added admin fields

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.utils.translation import gettext_lazy as _
 from core import models
 
 
@@ -12,6 +12,21 @@ class UserAdmin(BaseUserAdmin):
 
     ordering = ["id"]
     list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Important Dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
 
 
 admin.site.register(models.User, UserAdmin)
