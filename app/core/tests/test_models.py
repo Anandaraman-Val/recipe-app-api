8cbab0334cc665c4b029c472b74777e1a0fb1c02
Anandaraman-Val <anandaramanvallinayagam@gmail.com>
2024-08-03 17:30:50 -0700

    added model change

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,3 +35,8 @@ class ModelTest(TestCase):
 
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user("", "test123")
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser("test@example.com", "test123")
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
